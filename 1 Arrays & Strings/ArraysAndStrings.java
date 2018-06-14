2b772cb3dde60097a59712974bc239f53a523f28
SleekPanther <macrobyte2@gmail.com>
2018-06-14 17:43:46 -0400

    1.4 permutation of palindrome

--- 1 Arrays & Strings/ArraysAndStrings.java
+++ 1 Arrays & Strings/ArraysAndStrings.java
@@ -25,8 +25,6 @@ public class ArraysAndStrings {
 		return true;
 	}
 
-	//hints 1	84	122	131
-
 	//1.2 Given two strings, write a method to decide if one is a permutation of the other.
 	//O(n) ===(O(3n))		ignores lower/upper case	counts whitespace
 	public static boolean isPermutation(String str1, String str2){
@@ -106,19 +104,47 @@ public class ArraysAndStrings {
 		}
 		return builder.toString();
 	}
+	
+	//1.4 Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+	//n log n since sorting, use hash table for faster
+	//treats all characters including spaces as unique (potential to remove spaces if required)
+	public static boolean isPalindromePermutation(String str){
+		System.out.println("isPalindromePermutation? "+str);
+
+		str = str.toLowerCase();
+		char chars[] = str.toCharArray();
+		Arrays.sort(chars);
+		// System.out.println(Arrays.toString(chars));
+
+		for(int i=0; i<chars.length; i+=2){		//Loop over array by steps of 2 & compare to next thing in array
+			if(i+1==chars.length){	//break if array is an odd length since 1 lone character without a match is fine @ the end
+				return true;
+			}
+			if(chars[i]!=chars[i+1] && (chars.length%2)!=0){		//If extra character is in the middle, skip since it's allowed in an odd lengh string
+				i++;
+			}
+			if(chars[i]!=chars[i+1]){	//compare adjacent chars
+				return false;
+			}
+		}
+		return true;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("#1 Is unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?");
 		System.out.println(ArraysAndStrings.isUnique("abc"));
 		System.out.println(ArraysAndStrings.isUnique("abca2"));
-		
-		System.out.println("\n#1.3 Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the 'true' length of the string. (Note: If implementing in Java, please use a character array so that you can perform this operation in place.)");
-		ArraysAndStrings.URLify(new char[]{'M','r',' ','J','o','h','n',' ',' ','m','i','t','h',' ', ' ',' ', ' ',' ',' '},13);
-		System.out.println(ArraysAndStrings.URLifyOnePass("Mr John  mith",13));
 
 		System.out.println("\n1.2 Given two strings, write a method to decide if one is a permutation of the other.");
 		System.out.println(ArraysAndStrings.isPermutation("asd", "add"));
 		System.out.println(ArraysAndStrings.isPermutation("asd", "dsa"));
+
+		System.out.println("\n#1.3 Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the 'true' length of the string. (Note: If implementing in Java, please use a character array so that you can perform this operation in place.)");
+		ArraysAndStrings.URLify(new char[]{'M','r',' ','J','o','h','n',' ',' ','m','i','t','h',' ', ' ',' ', ' ',' ',' '},13);
+		System.out.println(ArraysAndStrings.URLifyOnePass("Mr John  mith",13));
+
+		System.out.println("\n1.4 Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome.");
+		System.out.println(ArraysAndStrings.isPalindromePermutation("tactcoapapa"));
 	}
 
 }
